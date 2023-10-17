8eefd88a4130feff12b8f6b174cf28767b066ba7
pasDamola <oyindadavid18@gmail.com>
2023-10-17 14:21:22 +0100

    getNextLocation command

--- main.go
+++ main.go
@@ -2,27 +2,76 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 )
 
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() string
+	callback    func()
 }
 
-func commandHelp() string {
- return  `
+type PokeAPI struct {
+	Count    int    `json:"count"`
+	Next     string `json:"next"`
+	Previous string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+func commandHelp() {
+ fmt.Println(  `
 Welcome to the Pokedex!
 Usage:
 
 help: Displays a help message
-exit: Exit the Pokedex`
+exit: Exit the Pokedex`)
 }
 
-func commandExit() string {
-	return "exit"
+func commandExit()  {
+
+}
+
+func getNextLocations()  {
+	
+	endpoint := fmt.Sprintf("https://pokeapi.co/api/v2/location?offset=%v&limit=20", )
+	resp, err := http.Get(endpoint)
+	if err != nil {
+		// handle error
+		log.Fatalf("couldn't read request because %v", err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+		log.Fatalf("couldn't read request because %v", err)
+	}
+
+	pokeAPI := PokeAPI{}
+
+	if json.Unmarshal(body, &pokeAPI); err != nil {
+		log.Fatalf("couldn't read request because %v", err)
+	}
+
+	next := pokeAPI.Next
+	results := pokeAPI.Results
+
+	for _, val := range results {
+		fmt.Println(val.Name)
+	}
+}
+
+
+func getPrevLocations()  {
 }
 
 
@@ -42,6 +91,16 @@ func main() {
 			description: "Exit the Pokedex",
 			callback:    commandExit,
 		},
+		"map": {
+			name: "map",
+			description: "Get next 20 locations from the PokeAPI",
+			callback: getNextLocations,
+		},
+		"mapb": {
+			name: "mapb",
+			description: "Get previous 20 locations from the PokeAPI",
+			callback: getPrevLocations,
+		},
 	}
 
 	for {
@@ -59,13 +118,8 @@ func main() {
 			_, ok := command[inputText]
 
 			if ok {
-				s := command[inputText].callback()
+				command[inputText].callback()
 				
-				if s == "exit" {
-					break
-				}
-
-				fmt.Println(s)
 
 			} else {
 				fmt.Printf("%s is not a valid command in the program\n", inputText)

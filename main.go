0869d4f0412f09ee3e5f181b3b24810898e37961
pasDamola <oyindadavid18@gmail.com>
2023-10-17 09:54:45 +0100

    building the repl

--- main.go
+++ main.go
@@ -6,8 +6,44 @@ import (
 	"os"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() string
+}
+
+func commandHelp() string {
+ return  `
+Welcome to the Pokedex!
+Usage:
+
+help: Displays a help message
+exit: Exit the Pokedex`
+}
+
+func commandExit() string {
+	return "exit"
+}
+
+
+
+
 
 func main() {
+
+	 command := map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+
 	for {
 
 		// Create a new scanner to read from standard input (os.Stdin)
@@ -19,7 +55,22 @@ func main() {
 		if scanner.Scan() {
 			// Use .Text() to get the input as a string
 			inputText := scanner.Text()
-			fmt.Printf("You entered: %s\n", inputText)
+
+			_, ok := command[inputText]
+
+			if ok {
+				s := command[inputText].callback()
+				
+				if s == "exit" {
+					break
+				}
+
+				fmt.Println(s)
+
+			} else {
+				fmt.Printf("%s is not a valid command in the program\n", inputText)
+			}
+			
 		} else {
 			fmt.Println("An error occurred while reading input.")
 		}

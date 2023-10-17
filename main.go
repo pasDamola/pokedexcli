4d92abd544a939103970861746c6812dbab87e75
pasDamola <oyindadavid18@gmail.com>
2023-10-17 08:43:27 +0100

    scanning input text from std input

--- main.go
+++ main.go
@@ -1,8 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 
 func main() {
-	fmt.Println("Hello world")
+	for {
+
+		// Create a new scanner to read from standard input (os.Stdin)
+		scanner := bufio.NewScanner(os.Stdin)
+   
+		fmt.Print("Pokedex > ")
+	
+		// Scan for input
+		if scanner.Scan() {
+			// Use .Text() to get the input as a string
+			inputText := scanner.Text()
+			fmt.Printf("You entered: %s\n", inputText)
+		} else {
+			fmt.Println("An error occurred while reading input.")
+		}
+	}
 }
